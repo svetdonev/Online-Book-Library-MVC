2ac5c04e81fbd6baeb0e7223199960773464ce9a
Svetlozar Donev <svetdonev1@gmail.com>
2024-09-16 01:26:34 +0300

    Initial commit

---
+++ Online-Book-Library-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_Book_Library_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
