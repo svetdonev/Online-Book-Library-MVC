3b178ce2a1d0253ee5c0d723908f06abc3005f48
Svetlozar Donev <svetdonev1@gmail.com>
2024-10-03 02:34:09 +0300

    Update book details view

--- Online-Book-Library-MVC/obj/Debug/net8.0/Online-Book-Library-MVC.AssemblyInfo.cs
+++ Online-Book-Library-MVC/obj/Debug/net8.0/Online-Book-Library-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Online-Book-Library-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85969f68bd64c72dcf81ff6d15fe9cfdbeb9530c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e28fdde268528562777162600a75015f84ddd37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Online-Book-Library-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Online-Book-Library-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
